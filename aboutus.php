2a65d30cb362f3f1e60c6e885e8116a121d4c337
pranjali0527 <68731759+pranjali0527@users.noreply.github.com>
2022-05-05 12:05:49 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,61 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col2">
+  <div id="gallery">
+    <ul>
+      <li class="placeholder" style="background-image:url(images/demo/image1.jpg);">Image Holder</li>
+     <!-- <li><a class="swap" style="background-image:url(images/demo/img3.jpg);" href="#gallery"><strong>  
+     Services</strong><span><img src="images/demo/image1.jpg" alt="" /></span></a></li>
+      <li><a class="swap" style="background-image:url(images/demo/img1.jpg);" href="#gallery"><strong>Products</strong><span><img src="#" alt="" /></span></a></li>
+      <li class="last"><a class="swap" style="background-image:url(images/demo/img2.jpg);" href="#gallery"><strong>Company</strong><span><img src="#" alt="" /></span></a></li>    -->
+    </ul>
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="wrapper col4">
+  <div id="container">
+    <div id="content">
+      <h1>About Us</h1>
+      <p align="justify">platform that brings together the best of healthcare offered by a curated network of partners for all stakeholders in the ecosystem.
+</p>
+      <p align="justify">...</p>
+      <p align="justify">...</p>
+      <div class="homecontent">
+        <ul>
+          <li>
+           <h2>Online Appointment Management</h2>
+            <p class="imgholder"><img src="images/spciality.jpg" alt="" style="width:286px;height:100px;"  /></p>
+          </li>
+          <li class="last">
+            <h2>24X7 services</h2>
+            <p class="imgholder"><img src="images/24x7.png" alt="" style="width:286px;height:100px;"   /></p>
+          </li>
+        </ul>
+        <div class="clear"></div>
+      </div>
+      <p><strong>......</strong><br />
+     ....</p>
+    </div>
+    <div id="column">
+      <div id="featured">
+        <ul>
+          <li>
+            <h2>Our Services</h2>
+            <p class="imgholder"><img src="images/health.jpg" alt="" style="width:240px;height:90px;" /></p>
+          </li>
+          <li role="menuitem">Amwell Services</li>
+ 
+          <li role="menuitem">Consultation</li>
+          <li role="menuitem">In Patient Services</li>
+          <li role="menuitem">Health Checkups</li>
+          <li role="menuitem">24 X 7 Services</li>
+        </ul>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
